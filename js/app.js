659ed5b1cc2260fec90454c269a890b097658a43
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2021-11-14 23:04:21 +0300

    Added conditional statement to Assign names

--- js/app.js
+++ js/app.js
@@ -84,6 +84,7 @@ const male_week_days_names ={Sunday:"Kwasi",Monday:"Kwadwo",Tuesday:"Kwabena", W
 const female_week_days_names ={Sunday:"Akosua",Monday:"Adwoa",Tuesday:"Abenaa", Wednesday:"Akua",Thursday:"Yaa",Friday:"Afua",Saturday:"Ama"};
  
 function get_my_akanName(){
+    var gender =Gender.value;
     var DD = parseInt(birth_Day.value);
     var MM =parseInt(birth_Month.value);
     var YY = parseInt(birth_Year.value.substr(2,4));
@@ -91,5 +92,35 @@ function get_my_akanName(){
 
     var day_of_the_week= Math.floor((((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10)) + DD )% 7);
 
-alert(day_of_the_week);
+    if (gender="male" && day_of_the_week === 0){
+        alert(male_week_days_names.Sunday)
+    }else if(gender="male" && day_of_the_week ===1){
+        alert(male_week_days_names.Monday);
+    }else if(gender="male" && day_of_the_week ===2){
+        alert(male_week_days_names.Tuesday);
+    }else if(gender="male" && day_of_the_week ===3){
+        alert(male_week_days_names.Wednesday);
+    }else if(gender="male" && day_of_the_week ===4){
+        alert(male_week_days_names.Thursday);
+    }else if(gender="male" && day_of_the_week ===5){
+        alert(male_week_days_names.Friday);
+    }else if(gender="male" && day_of_the_week ===6){
+        alert(male_week_days_names.Saturday);
+    }else if(gender="female" && day_of_the_week ===0){
+        alert(female_week_days_names.Sunday);
+    }else if(gender="female" && day_of_the_week ===1){
+        alert(female_week_days_names.Monday);
+    }else if(gender="female" && day_of_the_week ===2){
+        alert(female_week_days_names.Tuesday);
+    }else if(gender="female" && day_of_the_week ===3){
+        alert(female_week_days_names.Wednesday);
+    }else if(gender="female" && day_of_the_week ===4){
+        alert(female_week_days_names.Thursday);
+    }else if(gender==="female" && day_of_the_week ===5){
+        alert(female_week_days_names.Friday);
+    }else {
+        alert(female_week_days_names.Saturday);
+    }
+
+alert(Gender.value);
 }
